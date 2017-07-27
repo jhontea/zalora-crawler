af08c4d3807a3231d9cbcff55c861c8e16ff38f1
Hafizh <hafizh@suitmedia.com>
2017-07-27 14:27:28 +0700

    configure deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '153.92.8.52', user: 'u4555537', roles: %w{app web}, my_property: :my_value, port: 65002
+server '156.67.218.51', user: 'deploy', roles: %w{app web}, my_property: :my_value
 
-set :deploy_to, '/home/u4555537/www/zalora'
+set :deploy_to, '/home/deploy/www/zalora-crawler'
 
 # role-based syntax
 # ==================
